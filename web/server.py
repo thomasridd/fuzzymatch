7c67c7a980c869de8b4591f7ac47cc3aae21e4c5
thomasridd <thomasridd@hotmail.co.uk>
2014-11-29 16:20:47 +0000

    Commit all

--- web/server.py
+++ web/server.py
@@ -1,14 +1,16 @@
 __author__ = 'hadoop'
 
 import os
-from flask import Flask
+from flask import Flask, render_template, request
+from DataToolkits import FuzzyMatch
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello world!"
+def landing_page():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
